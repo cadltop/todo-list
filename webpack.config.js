e750a67ef0d2e503d5a80281c10f2e532e907268
cadltop <158861368+cadltop@users.noreply.github.com>
2024-07-25 13:22:04 -0500

    Configure html plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
   devtool: 'inline-source-map',
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Todo List',
+    template: 'src/index.html'
+  })],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
