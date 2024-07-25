a35935c49ad0441042a0f342414d63cf1ab151fa
cadltop <158861368+cadltop@users.noreply.github.com>
2024-07-25 13:35:37 -0500

    Set fonts assets.

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,12 @@ module.exports = {
     {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+    },
+    {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
     }]
-},
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
