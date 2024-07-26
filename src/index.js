fcb0d717510b3d129a74b86613983172787ea958
cadltop <158861368+cadltop@users.noreply.github.com>
2024-07-26 18:33:55 -0500

    Add background color change for .action and .project.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import 'normalize.css';
 import './index.css';
+
+const clickable = document.querySelectorAll('.action, .project');
+for(let item in clickable) {
+    clickable[item].addEventListener('mouseover', () => {
+        clickable[item].style = 'background-color: #c8f8d9;';
+    })
+    clickable[item].addEventListener('mouseout', () => {
+        clickable[item].style = 'background-color: ';
+    })
+}
