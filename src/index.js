ec6afd2ec07361d11b5a39ee3440fccd3eb79f48
cadltop <158861368+cadltop@users.noreply.github.com>
2024-07-27 15:56:50 -0500

    Remove task.js.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import 'normalize.css';
 import './index.css';
-import {Task} from './task.js';
 
 const clickable = document.querySelectorAll('.action, .project');
 for(let i = 0; i < clickable.length; i++) {
@@ -11,14 +10,3 @@ for(let i = 0; i < clickable.length; i++) {
         clickable[i].style = 'background-color: ';
     })
 }
-
-let todos = [];
-document.querySelector('form > button').addEventListener('click', (event) => {
-    event.preventDefault();
-    const title = document.querySelector('#title').value;
-    const description = document.querySelector('#description').value;
-    const dueDate = document.querySelector('#due-date').value;
-    const priority = document.querySelector('#priority').value;
-    todos.push(new Task(title, description, dueDate, priority));
-    console.log(todos);
-}, false)
