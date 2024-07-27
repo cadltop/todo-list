96dc47ab01a876af7965113d85a099e3cd3803ec
cadltop <158861368+cadltop@users.noreply.github.com>
2024-07-27 14:06:55 -0500

    Change loop type.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import 'normalize.css';
 import './index.css';
+import {Task} from './task.js';
 
 const clickable = document.querySelectorAll('.action, .project');
-for(let item in clickable) {
-    clickable[item].addEventListener('mouseover', () => {
-        clickable[item].style = 'background-color: #c8f8d9;';
+for(let i = 0; i < clickable.length; i++) {
+    clickable[i].addEventListener('mouseover', () => {
+        clickable[i].style = 'background-color: #c8f8d9;';
     })
-    clickable[item].addEventListener('mouseout', () => {
-        clickable[item].style = 'background-color: ';
+    clickable[i].addEventListener('mouseout', () => {
+        clickable[i].style = 'background-color: ';
     })
 }
+
+let todos = [];
+document.querySelector('form > button').addEventListener('click', (event) => {
+    event.preventDefault();
+    const title = document.querySelector('#title').value;
+    const description = document.querySelector('#description').value;
+    const dueDate = document.querySelector('#due-date').value;
+    const priority = document.querySelector('#priority').value;
+    todos.push(new Task(title, description, dueDate, priority));
+    console.log(todos);
+}, false)
