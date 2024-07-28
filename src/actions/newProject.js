6d409149a54661b543237a5910e203845c63b6b7
cadltop <158861368+cadltop@users.noreply.github.com>
2024-07-28 12:36:53 -0500

    Add newProject.js and import into index.js.

---
+++ src/actions/newProject.js
@@ -0,0 +1,5 @@
+import './actions.css';
+export function setNewProject(){
+    const header = document.querySelector('h1');
+    header.innerHTML = 'New Project';
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import 'normalize.css';
 import './index.css';
+
 import { setNewTask } from './actions/newTask.js';
+import { setNewProject } from './actions/newProject.js';
 
 const clickable = document.querySelectorAll('.action, .project');
 for(let i = 0; i < clickable.length; i++) {
@@ -11,6 +13,10 @@ for(let i = 0; i < clickable.length; i++) {
         clickable[i].style = 'background-color: ';
     })
 }
+
 clickable[0].addEventListener('click', () => {
     setNewTask();
+})
+clickable[1].addEventListener('click', () => {
+    setNewProject();
 })
