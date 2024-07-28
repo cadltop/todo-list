0bb090e67cb2a27784b6e76199c850f04b41af12
cadltop <158861368+cadltop@users.noreply.github.com>
2024-07-27 22:33:53 -0500

    Create form with labels, inputs and textarea.

--- src/new-task/nt.js
+++ src/new-task/nt.js
@@ -1,4 +1,42 @@
 export function setNewTask(){
+    const mainSection = document.querySelector('main');
     const header = document.querySelector('h1');
     header.innerHTML = 'New Task';
+
+    const form = document.createElement('form');
+    
+    const titleLabel = makeLabel('Title');
+    const titleInput = document.createElement('input');
+    titleInput.type = 'text';
+    titleInput.id = 'title';
+    
+    const descriptionLabel = makeLabel('Description');
+    const descriptionTextarea = document.createElement('textarea');
+    descriptionTextarea.id = 'description';
+
+    const dueDateLabel = makeLabel('due-date');
+    dueDateLabel.innerHTML = 'Due Date:';
+    const dueDateInput = document.createElement('input');
+    dueDateInput.type = 'date';
+    dueDateInput.id = 'due-date';
+
+    const priorityLabel = makeLabel('Priority');
+    const prioritySelect = document.createElement('select');
+    prioritySelect.id = 'priority';
+
+
+    mainSection.append(form);
+    form.append(titleLabel);
+    form.append(titleInput);
+    form.append(descriptionLabel);
+    form.append(descriptionTextarea);
+    form.append(dueDateLabel);
+    form.append(dueDateInput);
+
+    function makeLabel(value) {
+        const label = document.createElement('label');
+        label.htmlFor = value.toLowerCase();
+        label.innerHTML = `${value}:`
+        return label;
+    }
 }
