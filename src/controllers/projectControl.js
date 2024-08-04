30375be7a89e85116c1d887b0f5e8c4d0226c7fe
cadltop <158861368+cadltop@users.noreply.github.com>
2024-08-04 00:02:51 -0500

    Set window to push task into new project.

--- src/controllers/projectControl.js
+++ src/controllers/projectControl.js
@@ -6,10 +6,22 @@ const projectControl = (function() {
     const openWindow = newProject.renderWindow;
     newProject.saveButton.addEventListener('click', (event) => {
         const name = newProject.nameInput.value;
-        // const tasks = newProject.tasksSelect.value;inde  
+        const tasks = newProject.tasksInputs;
+        
         const project = new Project(name);
-        projects.push(project);
-        console.log(projects);
+        for(let checkBox of tasks) {
+            if(checkBox.checked) {
+                for(let task of projects[0].tasks){
+                    let titleToId = (task.title.match(' ') !== null) ? task.title.replace(' ', '-'): task.title;
+                    titleToId.toLowerCase();
+                    if(titleToId === checkBox.id){
+                        project.tasks.push(task);
+                    }
+                }
+            }
+        }
+        
+        
         event.preventDefault();
     })
     
