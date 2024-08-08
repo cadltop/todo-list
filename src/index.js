13bf91095e46c7a0abfa9002371963c58a0cb6f9
cadltop <158861368+cadltop@users.noreply.github.com>
2024-08-07 22:18:47 -0500

    Add event listeners to show options.

--- src/index.js
+++ src/index.js
@@ -11,23 +11,36 @@ for(let i = 0; i < clickable.length; i++) {
             clickable[i].addEventListener('click', () => {
                 emptyMain();
                 taskControl.openWindow();
-            });
+            })
             break;
         case 1:
             clickable[i].addEventListener('click', () => {
                 emptyMain();
                 projectControl.openWindow();
-            });
+            })
             break;
         default:
             clickable[i].addEventListener('click', () => {
                 emptyMain();
                 projectControl.openProject();
-            });
+            })
+            clickable[i].addEventListener('mouseenter', () => {
+                const editIcon = document.createElement('img');
+                editIcon.src = '../res/icons/pencil-box.svg';
+                editIcon.alt = 'edit icon';
+                const deleteIcon = document.createElement('img');
+                deleteIcon.src = '../res/icons/file-excel-box.svg';
+                deleteIcon.alt = 'delete icon';
+                clickable[i].append(editIcon, deleteIcon);
+            })
+            clickable[i].addEventListener('mouseleave', () => {
+                while(clickable[i].lastElementChild.tagName.toLowerCase() !== 'p'){
+                    clickable[i].lastElementChild.remove();
+                }
+            })
             break;
     }
 }
-
 function emptyMain() {
     const mainSection = document.querySelector('main');
     while(mainSection.lastChild !== document.querySelector('h1')){
