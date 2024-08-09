5731d8ef8080faa5c484ca595ab626c1b2727425
cadltop <158861368+cadltop@users.noreply.github.com>
2024-08-08 19:18:04 -0500

    Split clickable into actions and allTasks.

--- src/index.js
+++ src/index.js
@@ -4,46 +4,41 @@ import './index.css';
 import { taskControl } from './controllers/taskControl.js';
 import { projectControl } from './controllers/projectControl.js';
 
-const clickable = document.querySelectorAll('.action, .project');
-for(let i = 0; i < clickable.length; i++) {
-    switch(i)  {
-        case 0:
-            clickable[i].addEventListener('click', () => {
-                emptyMain();
-                taskControl.openWindow();
-            })
-            break;
-        case 1:
-            clickable[i].addEventListener('click', () => {
-                emptyMain();
-                projectControl.openWindow();
-            })
-            break;
-        default:
-            clickable[i].addEventListener('click', () => {
-                emptyMain();
-                projectControl.openProject();
-            })
-            clickable[i].addEventListener('mouseenter', () => {
-                const editIcon = document.createElement('img');
-                editIcon.src = '../res/icons/pencil-box.svg';
-                editIcon.alt = 'edit icon';
-                const deleteIcon = document.createElement('img');
-                deleteIcon.src = '../res/icons/file-excel-box.svg';
-                deleteIcon.alt = 'delete icon';
-                clickable[i].append(editIcon, deleteIcon);
-            })
-            clickable[i].addEventListener('mouseleave', () => {
-                while(clickable[i].lastElementChild.tagName.toLowerCase() !== 'p'){
-                    clickable[i].lastElementChild.remove();
-                }
-            })
-            break;
+const actions = document.querySelectorAll('.action');
+const allTasks = document.querySelector('.project.all');
+
+actions[0].addEventListener('click', () => {
+    emptyMain();
+    taskControl.openWindow();
+})
+actions[1].addEventListener('click', () => {
+    emptyMain();
+    projectControl.openWindow();
+})
+
+allTasks.addEventListener('click', () => {
+    emptyMain();
+    projectControl.openProject('All Tasks');
+})
+allTasks.addEventListener('mouseenter', () => {
+    const editIcon = document.createElement('img');
+    editIcon.src = '../res/icons/pencil-box.svg';
+    editIcon.alt = 'edit icon';
+    const deleteIcon = document.createElement('img');
+    deleteIcon.src = '../res/icons/file-excel-box.svg';
+    deleteIcon.alt = 'delete icon';
+    allTasks.append(editIcon, deleteIcon);
+})
+allTasks.addEventListener('mouseleave', () => {
+    while(allTasks.lastElementChild.tagName.toLowerCase() !== 'p'){
+        allTasks.lastElementChild.remove();
     }
-}
+})
+
 function emptyMain() {
     const mainSection = document.querySelector('main');
     while(mainSection.lastChild !== document.querySelector('h1')){
         mainSection.removeChild(mainSection.lastChild);
     }
 }
+export {emptyMain};
