194ea3b3b3c979007e6673b59fe5dd8728c4aa8e
cadltop <158861368+cadltop@users.noreply.github.com>
2024-08-17 12:23:49 -0500

    Implemment Form class on projec Edit.

--- src/objects/projectEdit.js
+++ src/objects/projectEdit.js
@@ -1,43 +1,24 @@
 import '../actions/actions.css';
-import { emptyMain } from '../index.js';
+import { Form } from '../classes/form.js';
 
-const projectEdit = (function() {
-    const form = document.createElement('form');
-
-    const nameLabel = makeLabel('Name');
-    const nameInput = makeInput('input', 'name', 'text');
-
-    const saveButton = document.createElement('button');
-    saveButton.type = 'submit';
-    saveButton.innerHTML = 'Save';
-
+export const projectEdit = (function() {
+    const form = new Form();
+    
+    const nameLabel = form.makeLabel('Name');
+    const nameInput = form.makeInput('name', 'text');
     let nameInitial = '';
+
+    const saveButton = form.makeButton('Save', 'submit');
+
     const renderWindow = function(projectName) {
-        const header = document.querySelector('h1');
-        header.innerHTML = '';
-        const mainSection = document.querySelector('main');
-        emptyMain();
         nameInput.value = projectName;
         projectEdit.nameInitial = projectName
-        mainSection.append(form);
 
-        form.append(nameLabel, nameInput);
-        form.append(saveButton);
+        form.clearMainAndHeader();
+        form.element.append(nameLabel, nameInput);
+        form.element.append(saveButton);
+        form.setMainAndHeader('');
     }
 
-    function makeLabel(forVal) {
-        const label = document.createElement('label');
-        label.htmlFor = forVal.toLowerCase();
-        label.innerHTML = forVal;
-        return label;
-    }
-    function makeInput(element, idVal, typeVal){
-        const input = document.createElement(`${element}`);
-        input.id = idVal;
-        if(typeVal) input.type = typeVal;
-        return input;
-    }
     return {nameInput, saveButton, renderWindow, nameInitial};
 })();
-
-export {projectEdit};
