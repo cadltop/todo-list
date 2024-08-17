93f21fe54788bfcef8613d57b16e53b9bfc7b0eb
cadltop <158861368+cadltop@users.noreply.github.com>
2024-08-16 22:44:38 -0500

    Create clearMainAndHeader method for new windows.

--- src/classes/mainWindow.js
+++ src/classes/mainWindow.js
@@ -1,13 +1,21 @@
 export class mainWindow extends Document {
     #main = document.querySelector('main');
     #header = document.querySelector('h1');
+
     constructor(element) {
         super();
         this.element = document.createElement(`${element}`);
     }
+    
     setMainAndHeader(headerText) {
         this.#header.innerHTML = headerText;
         this.#main.append(this.element);
+    }
+    clearMainAndHeader() {
+        while(this.#main.lastChild !== this.#header){
+            this.#main.lastChild.remove();
+        }
+        this.#header.innerHTML = '';
     }
     replaceSpace(string) {
         const newString = ((string.match(' ') !== null) 
