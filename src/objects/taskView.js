122325805f2b78703daaf305ee0b867fe1f4d7b8
cadltop <158861368+cadltop@users.noreply.github.com>
2024-08-17 12:38:37 -0500

    Implement ExtendeForm in taskView.

--- src/objects/taskView.js
+++ src/objects/taskView.js
@@ -1,71 +1,49 @@
 import '../actions/actions.css';
-import { emptyMain } from '../index.js';
+import { ExtendedForm } from '../classes/extendedForm.js';
 
 const taskView = (function() {
-    const form = document.createElement('form');
-
-    const titleLabel = makeLabel('Title');
-    const titleInput = makeInput('input', 'title', 'text');
+    const form = new ExtendedForm();
     
-    const descriptionLabel = makeLabel('Description');
-    const descriptionTextarea = makeInput('textarea', 'description', undefined);
-
-    const dueDateLabel = makeLabel('due-date');
-    dueDateLabel.innerHTML = 'Due Date';
-    const dueDateInput = makeInput('input', 'due-date', 'datetime-local');
-
-    const priorityLabel = makeLabel('Priority');
-    const prioritySelect = makeInput('select', 'priority', undefined);
-
-    const saveButton = document.createElement('button');
-    saveButton.type = 'submit';
-    saveButton.innerHTML = 'Save';
-    
+    const titleLabel = form.makeLabel('Title');
+    const titleInput = form.makeInput('title', 'text');
     let titleInitial = '';
+    
+    const descriptionLabel = form.makeLabel('Description');
+    const descriptionTextarea = form.makeTextarea('description');
+
+    const dueDateLabel = form.makeLabel('Due Date');
+    const dueDateInput = form.makeInput('due date', 'datetime-local');
+
+    const priorityLabel = form.makeLabel('Priority');
+    const prioritySelect = form.makeSelect('priority');
+
+    const lowOption = form.makeOption('Low');
+    const mediumOption = form.makeOption('Medium');
+    const highOption = form.makeOption('High');
+    prioritySelect.append(lowOption, mediumOption, highOption);
+
+    const saveButton = form.makeButton('Save', 'submit');
+    
     const renderWindow = function(taskTitle, taskDescription, taskDueDate, taskPriority) {
-        const header = document.querySelector('h1');
-        header.innerHTML = '';
-        const mainSection = document.querySelector('main');
-        emptyMain()
         titleInput.value = taskTitle;
         taskView.titleInitial = taskTitle;
         descriptionTextarea.value = taskDescription;
         dueDateInput.value = taskDueDate;
+
         const priorityOptions = prioritySelect.children;
         for(let option of priorityOptions) {
             if(option.value === taskPriority) option.selected = true;
         }
-        mainSection.append(form);
 
-        form.append(titleLabel, titleInput);
-        form.append(descriptionLabel, descriptionTextarea);
-        form.append(dueDateLabel, dueDateInput);
-        form.append(priorityLabel, prioritySelect);
-        form.append(saveButton);
+        form.clearMainAndHeader();
+        form.element.append(titleLabel, titleInput);
+        form.element.append(descriptionLabel, descriptionTextarea);
+        form.element.append(dueDateLabel, dueDateInput);
+        form.element.append(priorityLabel, prioritySelect);
+        form.element.append(saveButton);
+        form.setMainAndHeader('');
     }
-    function makeLabel(forVal) {
-        const label = document.createElement('label');
-        label.htmlFor = forVal.toLowerCase();
-        label.innerHTML = `${forVal}`
-        return label;
-    }
-    function makeInput(element, idVal, typeVal){
-        const input = document.createElement(`${element}`);
-        input.id = idVal;
-        if(typeVal) input.type = typeVal;
-        if(idVal === 'priority') {
-            const options = [makeOption('Low'), makeOption('Medium'), makeOption('High')];
-            input.append(options[0], options[1], options[2]);
-        }
-        function makeOption(valueVal){
-            const option = document.createElement('option');
-            let valueNew = (valueVal.match(' ') !== null) ? valueVal.replace(' ', '-'): valueVal;
-            option.value = valueNew.toLowerCase();
-            option.innerHTML = `${valueVal}`;
-            return option;
-        }
-        return input;
-    }
+    
     return {titleInput, descriptionTextarea, dueDateInput, 
         prioritySelect, saveButton, renderWindow, titleInitial};
 })();
