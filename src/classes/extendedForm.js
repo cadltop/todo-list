9460d7d295e91f6a1fdfa7bf6b8c06fa0911714b
cadltop <158861368+cadltop@users.noreply.github.com>
2024-08-16 22:20:36 -0500

    Extend Form class.

---
+++ src/classes/extendedForm.js
@@ -0,0 +1,33 @@
+import { Form } from "./form";
+export class ExtendedForm extends Form {
+    constructor() {
+        super();
+    }
+    makeCheckBoxList(listDiv, listItems) {
+        let inputList = [];
+        for(let item of listItems) {
+            if(item !== listItems[0]) {
+                const checkBox = this.makeCheckBox(`${item.name}`);
+                listDiv.append(checkBox.input, checkBox.label);
+                inputList.push(checkBox.input);
+            }
+        }
+        return inputList;
+    }
+    makeTextarea(id) {
+        const textarea = document.createElement('textarea');
+        textarea.id = this.replaceSpace(id);
+        return textarea;
+    }
+    makeSelect(id) {
+        const select = document.createElement('select');
+        select.id = this.replaceSpace(id);
+        return select;
+    }
+    makeOption(value) {
+        const option = document.createElement('option');
+        option.value = this.replaceSpace(value);
+        option.innerHTML = value;
+        return option;
+    }
+}
