9ed5c41b9bbd34e97fd7cab73248befe698e2437
cadltop <158861368+cadltop@users.noreply.github.com>
2024-08-16 22:26:55 -0500

    Implement ExtendedForm and datahandler on new Task.

--- src/actions/newTask.js
+++ src/actions/newTask.js
@@ -1,91 +1,47 @@
 import './actions.css';
-import { data } from '../data.js';
+import { ExtendedForm } from '../classes/extendedForm.js';
+import { dataHandler } from '../dataHandler';
 
-const newTask = (function() {
-    const form = document.createElement('form');
+export const newTask = (function() {
+    const form = new ExtendedForm();
     
-    const titleLabel = makeLabel('Title');
-    const titleInput = makeInput('input', 'title', 'text');
+    const titleLabel = form.makeLabel('Title');
+    const titleInput = form.makeInput('title', 'text');
     
-    const descriptionLabel = makeLabel('Description');
-    const descriptionTextarea = makeInput('textarea', 'description', undefined);
+    const descriptionLabel = form.makeLabel('Description');
+    const descriptionTextarea = form.makeTextarea('description');
 
-    const dueDateLabel = makeLabel('due-date');
-    dueDateLabel.innerHTML = 'Due Date';
-    const dueDateInput = makeInput('input', 'due-date', 'datetime-local');
+    const dueDateLabel = form.makeLabel('Due Date');
+    const dueDateInput = form.makeInput('due date', 'datetime-local');
 
-    const priorityLabel = makeLabel('Priority');
-    const prioritySelect = makeInput('select', 'priority', undefined);
+    const priorityLabel = form.makeLabel('Priority');
+    const prioritySelect = form.makeSelect('priority');
 
-    const projectLabel = makeLabel('Project');
-    const projectsContainer = makeInput('div', 'projects', undefined);
+    const lowOption = form.makeOption('Low');
+    const mediumOption = form.makeOption('Medium');
+    const highOption = form.makeOption('High');
+    prioritySelect.append(lowOption, mediumOption, highOption);
+
+    const projectsLabel = form.makeLabel('Projects');
+    const projectsDiv = form.makeDiv('projects');
     let projectsInputs = [];
 
-    const saveButton = document.createElement('button');
-    saveButton.type = 'submit';
-    saveButton.innerHTML = 'Save';
+    const saveButton = form.makeButton('Save', 'submit');
     
     const renderWindow = function() {
-        while(projectsContainer.lastChild){
-            projectsContainer.removeChild(projectsContainer.lastChild);
-        }
-        for(let project of data.projects) {
-            if(project !== data.projects[0]) {
-                const checkBox = makeCheckBox(`${project.name}`);
-                projectsContainer.append(checkBox.input, checkBox.label);
-            }
-        }
-        for(let child of projectsContainer.children){
-            if(child.tagName.toLowerCase() === 'input') {
-                projectsInputs.push(child);
-            }
-        }
+        const projects = dataHandler.getAllProjects();
+        form.emptyDiv(projectsDiv);
+        projectsInputs = form.makeCheckBoxList(projectsDiv, projects);
 
-        const header = document.querySelector('h1');
-        header.innerHTML = 'New Task';
-        const mainSection = document.querySelector('main');
-        mainSection.append(form);
-
-        form.append(titleLabel, titleInput);
-        form.append(descriptionLabel, descriptionTextarea);
-        form.append(dueDateLabel, dueDateInput);
-        form.append(priorityLabel, prioritySelect);
-        if(data.projects.length > 1) {form.append(projectLabel, projectsContainer)}
-        form.append(saveButton);
-    }
-    function makeLabel(forVal) {
-        const label = document.createElement('label');
-        label.htmlFor = forVal.toLowerCase();
-        label.innerHTML = `${forVal}`
-        return label;
-    }
-    function makeInput(element, idVal, typeVal){
-        const input = document.createElement(`${element}`);
-        input.id = idVal;
-        if(typeVal) input.type = typeVal;
-        if(idVal === 'priority') {
-            const options = [makeOption('Low'), makeOption('Medium'), makeOption('High')];
-            input.append(options[0], options[1], options[2]);
-        }
-        function makeOption(valueVal){
-            const option = document.createElement('option');
-            let valueNew = (valueVal.match(' ') !== null) ? valueVal.replace(' ', '-'): valueVal;
-            option.value = valueNew.toLowerCase();
-            option.innerHTML = `${valueVal}`;
-            return option;
-        }
-        return input;
-    }
-    function makeCheckBox(idVal){
-        const input = document.createElement('input');
-        input.type = "checkbox";
-        let idNew = (idVal.match(' ') !== null) ? idVal.replace(' ', '-'): idVal;
-        input.id = idNew.toLowerCase();
-        const label = makeLabel(idVal);
-        return {input, label};
+        form.element.append(titleLabel, titleInput);
+        form.element.append(descriptionLabel, descriptionTextarea);
+        form.element.append(dueDateLabel, dueDateInput);
+        form.element.append(priorityLabel, prioritySelect);
+        if(projects.length > 1) form.element.append(projectsLabel, projectsDiv);
+        form.element.append(saveButton);
+        form.setMainAndHeader('New Task');
     }
 
     return {titleInput, descriptionTextarea, dueDateInput, 
         prioritySelect, projectsInputs, saveButton, renderWindow};
 })();
-export {newTask}
