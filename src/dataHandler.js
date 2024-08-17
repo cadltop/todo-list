c657fb2fa8b2099451930212085c7493ed1238f5
cadltop <158861368+cadltop@users.noreply.github.com>
2024-08-16 22:21:36 -0500

    Add getAllProjects method to dataHandler.

--- src/dataHandler.js
+++ src/dataHandler.js
@@ -4,6 +4,9 @@ export const dataHandler = {
     getAllTasks() {
         return data.projects[0].tasks;
     },
+    getAllProjects() {
+        return data.projects;
+    },
     updateProjects(project){
         data.newProjects = project;
     }
