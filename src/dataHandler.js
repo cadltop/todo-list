892b9a443f7ee07eb21abd428117d514bac09efe
cadltop <158861368+cadltop@users.noreply.github.com>
2024-08-17 21:32:38 -0500

    Add deleteTask method to dataHandler.

--- src/dataHandler.js
+++ src/dataHandler.js
@@ -1,5 +1,15 @@
 import { data } from "./data";
 export const dataHandler = {
+    deleteTask(taskTitle, project){
+        for(let t = 0; t < project.tasks.length; t++) {
+            if(project.tasks[t].title === taskTitle){
+                delete project.tasks[t];
+                projects.tasks = project.tasks.filter((value) => {
+                    return value !== undefined;
+                })                
+            }
+        }
+    },
     getAllTasks() {
         return data.projects[0].tasks;
     },
