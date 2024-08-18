f07e6eedb7c9221b1d65f070348aa291674e4843
cadltop <158861368+cadltop@users.noreply.github.com>
2024-08-17 19:08:35 -0500

    Implement List class and datahandler object in projectView.

--- src/objects/projectView.js
+++ src/objects/projectView.js
@@ -1,25 +1,21 @@
 import './projectView.css';
-import {data} from '../data.js';
+import { List } from '../classes/list.js';
+import { dataHandler } from '../dataHandler.js';
 import { taskEdit } from './taskEdit.js';
 
-const projectView = (function(){
-    const list = document.createElement('ul');
+export const projectView = (function(){
+    const list = new List();
 
     const renderWindow = function(projectName) {
-        const project = (function(){
-            let project;
-            data.projects.forEach((value) => {
-                if(value.name === projectName) project = value;
-            });
-            return project;
-        })();
-        while(list.lastChild){
-            list.removeChild(list.lastChild);
-        }
+        list.clearMainAndHeader();
+        const project = dataHandler.getProject(projectName);
+        list.emptyElement(list.element);
+        
         const itemsAmmount = project.tasks.length;
         for(let i = 0; i < itemsAmmount; i++) {
             const listItem = document.createElement('li');
-            switch(project.tasks[i].priority) {
+            const task = project.tasks[i];
+            switch(task.priority) {
                 case 'low':
                     listItem.style.backgroundColor = '#baffac';
                     break;
@@ -31,88 +27,60 @@ const projectView = (function(){
                     break;
             }
 
-            const checkBox = document.createElement('input');
-            checkBox.type = 'checkbox';
-            if(project.tasks[i].checked) checkBox.checked = true;
+            const checkBox = list.makeInput(undefined, 'checkbox');
+            if(task.checked) checkBox.checked = true;
             checkBox.addEventListener('click', () => {
                 if(checkBox.checked) {
-                    project.tasks[i].checked = true;
+                    task.checked = true;
                 } else {
-                    project.tasks[i].checked = false;
+                    task.checked = false;
                 }
             })
 
-            const textDiv = makeDiv('text');
+            const textDiv = list.makeDiv(undefined, 'text');
+            const titleDiv = list.makeDiv(undefined, 'title');
+            const titleP = list.makeP(task.title, undefined);
+            const titleImg = list.makeImg('../res/icons/text-box.svg', 'tasks icon');
+            titleDiv.append(titleP, titleImg);
+            const dueDateP = list.makeP(task.dueDate, 'due-date');
+            textDiv.append(titleDiv, dueDateP);
 
-            const titleDiv = makeDiv('title');
-            const titleP = makeP(project.tasks[i].title, undefined);
-            const titleImg = makeImg('../res/icons/text-box.svg', 'tasks icon');
+            const optionsDiv = list.makeDiv(undefined, 'options');
+            const editImg = list.makeImg('../res/icons/pencil-box.svg', 'edit icon');
+            const deleteImg = list.makeImg('../res/icons/file-excel-box.svg', 'delete icon');
+            optionsDiv.append(editImg, deleteImg);
 
-            const dueDateP = makeP(project.tasks[i].dueDate, 'due-date');
-
-            const optionsDiv = makeDiv('options');
-            const editImg = makeImg('../res/icons/pencil-box.svg', 'edit icon');
-            const deleteImg = makeImg('../res/icons/file-excel-box.svg', 'delete icon');
-
-            editImg.addEventListener('click', () => viewInfo());
+            editImg.addEventListener('click', () => {
+                taskEdit.renderWindow(task.title, task.description, task.dueDate, task.priority);
+            });
             deleteImg.addEventListener('click', () => {
                 if(projectName === 'All Tasks'){
-                    for(let p = 1; p < data.projects.length; p++){
-                        for(let t = 0; t < data.projects[p].tasks.length; t++) {
-                            if(data.projects[p].tasks[t].title === project.tasks[i].title){
-                                delete data.projects[p].tasks[t];
-                                const newtasks = data.projects[p].tasks.filter((value) => {
+                    const projects = dataHandler.getAllProjects();
+                    for(let p = 1; p < projects.length; p++){
+                        for(let t = 0; t < projects[p].tasks.length; t++) {
+                            if(projects[p].tasks[t].title === project.tasks[i].title){
+                                delete projects[p].tasks[t];
+                                projects[p].tasks = projects[p].tasks.filter((value) => {
                                     return value !== undefined;
-                                })
-                                data.projects[p].tasks = newtasks;                
+                                })                
                             }
                         }
                     }
                 }
                 delete project.tasks[i];
-                const newtasks = project.tasks.filter((value) => {
+                project.tasks = project.tasks.filter((value) => {
                     return value !== undefined;
                 })
-                project.tasks = newtasks;
-                list.removeChild(listItem);
+                list.element.removeChild(listItem);
             })
-            function viewInfo(){
-                taskEdit.renderWindow(project.tasks[i].title, project.tasks[i].description, project.tasks[i].dueDate, project.tasks[i].priority);
-            }
 
             listItem.append(checkBox);
-            titleDiv.append(titleP, titleImg);
-            textDiv.append(titleDiv, dueDateP);
             listItem.append(textDiv);
-            optionsDiv.append(editImg, deleteImg);
             listItem.append(optionsDiv);
-            list.append(listItem);
+            list.element.append(listItem);
         }
-        const header = document.querySelector('h1');
-        header.innerHTML = projectName;
-        const mainSection = document.querySelector('main');
-
-        if(itemsAmmount > 0) mainSection.append(list);
-    }
-    
-    function makeDiv(classVal) {
-        const div = document.createElement('div');
-        div.classList.add(classVal);
-        return div;
-    }
-    function makeP(text, classVal) {
-        const paragraph = document.createElement('p');
-        paragraph.innerHTML = text;
-        if(classVal) paragraph.classList.add = classVal;
-        return paragraph
-    }
-    function makeImg(srcVal, altVal) {
-        const image = document.createElement('img');
-        image.src = srcVal;
-        image.alt = altVal;
-        return image
+        list.setMainAndHeader(projectName);
     }
     
     return {renderWindow};
 })();
-export {projectView};
